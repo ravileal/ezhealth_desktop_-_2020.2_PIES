e889805b642a1ec7d6c6048f3430cec15ae4fe01
Ravi Leal <42724238+ravileal@users.noreply.github.com>
2021-03-07 21:56:52 -0300

    Correção no controller de alimentos
    Co-Authored-By: Pedro Anderson <pedroanderson10@gmail.com>

--- src/Controller/ControllerAlimento.java
+++ src/Controller/ControllerAlimento.java
@@ -3,37 +3,39 @@ package Controller;
 import java.util.ArrayList;
 
 import Model.Alimento;
+import Repository.RepositorioAlimento;
+import Repository.RepositorioUsuario;
 import Util.*;
 
 public class ControllerAlimento implements IAlimentacao<Alimento>, CRUD<Alimento> {
 
-	private ControllerAlimento rep;
-	
+	private RepositorioAlimento rep;
+
 	public ControllerAlimento() {
-		rep = new ControllerAlimento();
+		rep = new RepositorioAlimento();
 	}
-	
-	/* -----------------------------
-	 * 		Métodos do CRUD
-	 * -----------------------------*/
+
+	/*
+	 * ----------------------------- Métodos do CRUD -----------------------------
+	 */
 	@Override
 	public boolean adicionar(Alimento obj) {
-		return (obj.getNome().equals(""))? false : rep.adicionar(obj);
+		return (obj.getNome().equals("")) ? false : rep.adicionar(obj);
 	}
 
 	@Override
 	public ArrayList<Alimento> buscar(String nome) {
-		return (nome.equals(""))? null: rep.buscar(nome);
+		return (nome.equals("")) ? null : rep.buscar(nome);
 	}
 
 	@Override
 	public boolean editar(String nome, Alimento obj) {
-		return (nome.equals(""))? false: rep.editar(nome, obj);
+		return (nome.equals("")) ? false : rep.editar(nome, obj);
 	}
 
 	@Override
 	public boolean remover(String nome) {
-		return (nome.equals(""))? false: rep.remover(nome);
+		return (nome.equals("")) ? false : rep.remover(nome);
 	}
 
 	@Override
