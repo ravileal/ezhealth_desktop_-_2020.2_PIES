865c7f318fc40913cadb7d1a603a8384ef74782b
Ravi Leal <42724238+ravileal@users.noreply.github.com>
2021-03-09 19:17:42 -0300

    Criação do layout principal

---
+++ src/View/LayoutMain.java
@@ -0,0 +1,206 @@
+package View;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Panel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
+
+import Util.ViewUtils;
+
+public class LayoutMain {
+	
+	private JFrame frame;
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					LayoutMain window = new LayoutMain();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	/**
+	 * @wbp.parser.entryPoint
+	 */
+	public LayoutMain() {
+		this.frame = new JFrame();
+		this.configureFrame();
+		this.configurePanelHeader();
+		this.configurePanelMenuBar();
+	}
+	
+	private void configureFrame() {
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 1058, 603);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setUndecorated(true);
+		frame.getContentPane().setLayout(null);
+		
+		ArrayList<Image> icons = new ArrayList<Image>();
+		icons.add(new ImageIcon(this.getClass().getResource("/Images/hospital.png")).getImage());
+		frame.setIconImages(icons);
+	}
+	
+	private void configurePanelHeader() {
+		Panel panel = new Panel();
+		panel.setBackground(Color.decode("#2F3542"));
+		panel.setBounds(0, 0, 1058, 107);
+		panel.setLayout(null);		
+		new ViewUtils().configureTitleBarAlternative(frame, panel, "#2F3542", "#FFFFFF", false);
+		
+		JLabel lblImgHospital = new JLabel(""); 
+		lblImgHospital.setBounds(43, 30, 52, 51);
+		new ViewUtils().setImageInLabel("/Images/hospital.png", lblImgHospital, panel);
+		
+		JLabel lblNewLabel_title = new JLabel("EZHEALTH");
+		lblNewLabel_title.setFont(new Font("Quicksand Medium", Font.PLAIN, 16));
+		lblNewLabel_title.setForeground(Color.decode("#A4B0BE"));
+		lblNewLabel_title.setBounds(112, 41, 96, 25);
+		panel.add(lblNewLabel_title);
+		
+		JLabel lblImgMinhaConta = new JLabel(""); 
+		lblImgMinhaConta.setBounds(930, 43, 39, 40);
+		new ViewUtils().setImageInLabel("/Images/accountWhite.png", lblImgMinhaConta, panel);
+		
+		JLabel lblNewLabel_minhaConta = new JLabel("<html>Minha<br>Conta</html>");
+		lblNewLabel_minhaConta.setFont(new Font("Quicksand Medium", Font.PLAIN, 18));
+		lblNewLabel_minhaConta.setForeground(Color.decode("#A4B0BE"));
+		lblNewLabel_minhaConta.setBounds(978, 39, 70, 51);
+		panel.add(lblNewLabel_minhaConta);
+		
+		frame.getContentPane().add(panel);
+	}
+	
+	private void configurePanelMenuBar() {
+		Panel panel = new Panel();
+		panel.setBackground(Color.decode("#A4B0BE"));
+		panel.setBounds(0, 106, 136, 497);
+		panel.setLayout(null);
+		
+		JLabel labelHome = new JLabel("Home");
+		labelHome.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				frame.dispose();
+				TelaHome.main(null);
+			}
+		});
+		
+		JLabel lblImgHome = new JLabel(""); 
+		lblImgHome.setBounds(10, 70, 19, 18);
+		new ViewUtils().setImageInLabel("/Images/restaurant.png", lblImgHome, panel);
+		
+		
+		labelHome.setFont(new Font("Quicksand Light", Font.PLAIN, 13));
+		labelHome.setBounds(36, 33, 87, 27);
+		panel.add(labelHome);
+		
+		JLabel labelRefeicoes = new JLabel("<html>Refei\u00E7\u00F5es <br> Personalizadas</html>");
+		labelRefeicoes.setHorizontalAlignment(SwingConstants.LEFT);
+		labelRefeicoes.setFont(new Font("Quicksand Light", Font.PLAIN, 13));
+		labelRefeicoes.setBounds(36, 65, 98, 35);
+		panel.add(labelRefeicoes);
+		labelRefeicoes.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				frame.dispose();
+				TelaRefeicoesPersonalizadas.main(null);
+			}
+		});
+		
+		JLabel lblImgRefeicao = new JLabel(""); 
+		lblImgRefeicao.setBounds(10, 115, 19, 18);
+		new ViewUtils().setImageInLabel("/Images/fitness.png", lblImgRefeicao, panel);
+		
+		JLabel lblNewLabel_7 = new JLabel("Menu");
+		lblNewLabel_7.setVerticalAlignment(SwingConstants.TOP);
+		lblNewLabel_7.setFont(new Font("Quicksand Medium", Font.PLAIN, 14));
+		lblNewLabel_7.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_7.setBounds(10, 11, 63, 30);
+		panel.add(lblNewLabel_7);
+		
+		JLabel labelExercicios = new JLabel("Exerc\u00EDcios");
+		labelExercicios.setHorizontalAlignment(SwingConstants.LEFT);
+		labelExercicios.setFont(new Font("Quicksand Light", Font.PLAIN, 13));
+		labelExercicios.setBounds(36, 106, 87, 35);
+		panel.add(labelExercicios);
+		labelExercicios.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				frame.dispose();
+				new TelaExercicios();
+				TelaExercicios.main(null);
+			}
+		});
+		
+		
+		JLabel lblImgExercicio = new JLabel(""); 
+		lblImgExercicio.setBounds(10, 36, 19, 18);
+		new ViewUtils().setImageInLabel("/Images/home.png", lblImgExercicio, panel);
+		
+		
+		JLabel lblNewLabel_7_1 = new JLabel("Mais op\u00E7\u00F5es");
+		lblNewLabel_7_1.setVerticalAlignment(SwingConstants.TOP);
+		lblNewLabel_7_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_7_1.setFont(new Font("Quicksand Medium", Font.PLAIN, 14));
+		lblNewLabel_7_1.setBounds(10, 151, 124, 30);
+		panel.add(lblNewLabel_7_1);
+		
+		
+		JLabel labelMeusDados = new JLabel("Meus Dados");
+		labelMeusDados.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				frame.dispose();
+				new TelaMeusDados();
+				TelaMeusDados.main(null);
+			}
+		});
+		labelMeusDados.setHorizontalAlignment(SwingConstants.LEFT);
+		labelMeusDados.setFont(new Font("Quicksand Light", Font.PLAIN, 13));
+		labelMeusDados.setBounds(36, 177, 87, 35);
+		panel.add(labelMeusDados);
+		
+		JLabel lblImgMeusDados = new JLabel(""); 
+		lblImgMeusDados.setBounds(10, 184, 19, 18);
+		new ViewUtils().setImageInLabel("/Images/account.png", lblImgMeusDados, panel);
+		
+		JLabel labelSair = new JLabel("Sair");
+		labelSair.setHorizontalAlignment(SwingConstants.LEFT);
+		labelSair.setFont(new Font("Quicksand Light", Font.PLAIN, 13));
+		labelSair.setBounds(36, 211, 87, 35);
+		labelSair.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.exit(0);
+			}
+		});
+		panel.add(labelSair);
+		
+		JLabel lblImgSair = new JLabel(""); 
+		lblImgSair.setBounds(10, 218, 19, 18);
+		new ViewUtils().setImageInLabel("/Images/exit.png", lblImgSair, panel);
+		
+		frame.getContentPane().add(panel);
+	}
+	
+	public JFrame getFrame() {
+		return this.frame;
+	}
+}
--- src/View/TelaHome.java
+++ src/View/TelaHome.java
@@ -2,28 +2,18 @@ package View;
 
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.Font;
-import java.awt.Image;
-
-import javax.swing.ImageIcon;
-import Util.ViewUtils;
 
 import java.awt.Panel;
 import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.ArrayList;
 
-import javax.swing.SwingConstants;
 import javax.swing.UIManager;
-import javax.swing.WindowConstants;
 import javax.swing.JPanel;
 
-public class TelaHome {
-
-	private JFrame frame;
+public class TelaHome extends LayoutMain{
 
 	/**
 	 * Launch the application.
@@ -34,7 +24,7 @@ public class TelaHome {
 			public void run() {
 				try {
 					TelaHome window = new TelaHome();
-					window.frame.setVisible(true);
+					window.getFrame().setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -46,6 +36,7 @@ public class TelaHome {
 	 * Create the application.
 	 */
 	public TelaHome() {
+		super();
 		initialize();
 	}
 
@@ -53,203 +44,37 @@ public class TelaHome {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setResizable(false);
-		frame.setBounds(100, 100, 1058, 603);
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame.setUndecorated(true);
-		frame.setTitle("Home - EzHealth");
-		frame.setLayout(null);
-		
-		ArrayList<Image> icons = new ArrayList<Image>();
-		icons.add(new ImageIcon(this.getClass().getResource("/Images/hospital.png")).getImage());
-		frame.setIconImages(icons);
-		
-		frame.getContentPane().add(configurePanelHeader());
-		frame.getContentPane().add(configurePanelMenuBar());
-		frame.getContentPane().add(configurePanelContent());
+		getFrame().setTitle("Home - EzHealth");
+		getFrame().getContentPane().add(configurePanelContent());
 	}
-	
-	private Panel configurePanelHeader() {
-		Panel panel = new Panel();
-		panel.setBackground(Color.decode("#2F3542"));
-		panel.setBounds(0, 0, 1058, 107);
-		panel.setLayout(null);		
-		
-		new ViewUtils().configureTitleBarAlternative(frame, panel, "#2F3542", "#FFFFFF", false);
-		
-		
-		JLabel lblImgHospital = new JLabel(""); 
-		lblImgHospital.setBounds(43, 30, 52, 51);
-		
-		new ViewUtils().setImageInLabel("/Images/hospital.png", lblImgHospital, panel);
-		
-		JLabel lblNewLabel_title = new JLabel("EZHEALTH");
-		lblNewLabel_title.setFont(new Font("Quicksand Medium", Font.PLAIN, 16));
-		lblNewLabel_title.setForeground(Color.decode("#A4B0BE"));
-		lblNewLabel_title.setBounds(112, 41, 96, 25);
-		panel.add(lblNewLabel_title);
-
-		
-		JLabel lblImgMinhaConta = new JLabel(""); 
-		lblImgMinhaConta.setBounds(930, 43, 39, 40);
-		
-		new ViewUtils().setImageInLabel("/Images/accountWhite.png", lblImgMinhaConta, panel);
-		
-		JLabel lblNewLabel_minhaConta = new JLabel("<html>Minha<br>Conta</html>");
-		lblNewLabel_minhaConta.setFont(new Font("Quicksand Medium", Font.PLAIN, 18));
-		lblNewLabel_minhaConta.setForeground(Color.decode("#A4B0BE"));
-		lblNewLabel_minhaConta.setBounds(978, 39, 70, 51);
-		panel.add(lblNewLabel_minhaConta);
-		
-		return panel;
-	}
-	
-	
-	private Panel configurePanelMenuBar() {
-		Panel panel = new Panel();
-		panel.setBackground(Color.decode("#A4B0BE"));
-		panel.setBounds(0, 106, 136, 497);
-		panel.setLayout(null);
-		
-		JLabel labelHome = new JLabel("Home");
-		labelHome.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// código para atualizar a tela
-			}
-		});
-		
-		JLabel lblImgHome = new JLabel(""); 
-		lblImgHome.setBounds(10, 70, 19, 18);
-		new ViewUtils().setImageInLabel("/Images/restaurant.png", lblImgHome, panel);
-		
-		
-		labelHome.setFont(new Font("Quicksand Light", Font.PLAIN, 13));
-		labelHome.setBounds(36, 33, 87, 27);
-		panel.add(labelHome);
-		
-		JLabel labelRefeicoes = new JLabel("<html>Refei\u00E7\u00F5es <br> Personalizadas</html>");
-		labelRefeicoes.setHorizontalAlignment(SwingConstants.LEFT);
-		labelRefeicoes.setFont(new Font("Quicksand Light", Font.PLAIN, 13));
-		labelRefeicoes.setBounds(36, 65, 98, 35);
-		panel.add(labelRefeicoes);
-		labelRefeicoes.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				frame.dispose();
-				new TelaRefeicoesPersonalizadas();
-				TelaRefeicoesPersonalizadas.main(null);
-			}
-		});
-		
-		JLabel lblImgRefeicao = new JLabel(""); 
-		lblImgRefeicao.setBounds(10, 115, 19, 18);
-		new ViewUtils().setImageInLabel("/Images/fitness.png", lblImgRefeicao, panel);
-		
-		JLabel lblNewLabel_7 = new JLabel("Menu");
-		lblNewLabel_7.setVerticalAlignment(SwingConstants.TOP);
-		lblNewLabel_7.setFont(new Font("Quicksand Medium", Font.PLAIN, 14));
-		lblNewLabel_7.setHorizontalAlignment(SwingConstants.LEFT);
-		lblNewLabel_7.setBounds(10, 11, 63, 30);
-		panel.add(lblNewLabel_7);
-		
-		JLabel labelExercicios = new JLabel("Exerc\u00EDcios");
-		labelExercicios.setHorizontalAlignment(SwingConstants.LEFT);
-		labelExercicios.setFont(new Font("Quicksand Light", Font.PLAIN, 13));
-		labelExercicios.setBounds(36, 106, 87, 35);
-		panel.add(labelExercicios);
-		labelExercicios.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				frame.dispose();
-				new TelaExercicios();
-				TelaExercicios.main(null);
-			}
-		});
-		
-		
-		JLabel lblImgExercicio = new JLabel(""); 
-		lblImgExercicio.setBounds(10, 36, 19, 18);
-		new ViewUtils().setImageInLabel("/Images/home.png", lblImgExercicio, panel);
-		
-		
-		JLabel lblNewLabel_7_1 = new JLabel("Mais op\u00E7\u00F5es");
-		lblNewLabel_7_1.setVerticalAlignment(SwingConstants.TOP);
-		lblNewLabel_7_1.setHorizontalAlignment(SwingConstants.LEFT);
-		lblNewLabel_7_1.setFont(new Font("Quicksand Medium", Font.PLAIN, 14));
-		lblNewLabel_7_1.setBounds(10, 151, 124, 30);
-		panel.add(lblNewLabel_7_1);
-		
-		
-		JLabel labelMeusDados = new JLabel("Meus Dados");
-		labelMeusDados.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				frame.dispose();
-				new TelaMeusDados();
-				TelaMeusDados.main(null);
-			}
-		});
-		labelMeusDados.setHorizontalAlignment(SwingConstants.LEFT);
-		labelMeusDados.setFont(new Font("Quicksand Light", Font.PLAIN, 13));
-		labelMeusDados.setBounds(36, 177, 87, 35);
-		panel.add(labelMeusDados);
-		
-		JLabel lblImgMeusDados = new JLabel(""); 
-		lblImgMeusDados.setBounds(10, 184, 19, 18);
-		new ViewUtils().setImageInLabel("/Images/account.png", lblImgMeusDados, panel);
-		
-		JLabel labelSair = new JLabel("Sair");
-		labelSair.setHorizontalAlignment(SwingConstants.LEFT);
-		labelSair.setFont(new Font("Quicksand Light", Font.PLAIN, 13));
-		labelSair.setBounds(36, 211, 87, 35);
-		labelSair.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				System.exit(0);
-			}
-		});
-		panel.add(labelSair);
-		
-		JLabel lblImgSair = new JLabel(""); 
-		lblImgSair.setBounds(10, 218, 19, 18);
-		new ViewUtils().setImageInLabel("/Images/exit.png", lblImgSair, panel);
-		
-		return panel;
-	}
-	
-	
 	
 	private JPanel configurePanelContent() {
 		JPanel panel = new JPanel();
 		panel.setBounds(136, 106, 922, 497);
 		panel.setBackground(Color.decode("#DFE4EA"));
 		panel.setLayout(null);
-		
-		JLabel labelMes = new JLabel("Fevereiro 2021");
-		labelMes.setBounds(20, 11, 125, 45);
-		panel.add(labelMes);
-		labelMes.setFont(new Font("Quicksand Medium", Font.PLAIN, 16));
-		
-		JLabel labelData = new JLabel("Segunda - 10/02");
-		labelData.setBounds(20, 43, 125, 22);
-		panel.add(labelData);
-		labelData.setFont(new Font("Quicksand Light", Font.PLAIN, 12));
-		
-		JLabel lblNewLabel_4_1 = new JLabel("Refei\u00E7\u00F5es");
-		lblNewLabel_4_1.setBounds(32, 291, 125, 33);
-		panel.add(lblNewLabel_4_1);
-		lblNewLabel_4_1.setFont(new Font("Quicksand Medium", Font.PLAIN, 16));
 				
-		panel.add(configurePanelCalorias());
-		
+		this.configureData(panel);
+		this.configurePanelCalorias(panel);
 		this.configureButtonsRefeicoes(panel);
 		
 		return panel;
 	}
 	
-	private Panel configurePanelCalorias() {
+	private void configureData(JPanel panel_content) {
+		JLabel labelMes = new JLabel("Fevereiro 2021");
+		labelMes.setBounds(20, 11, 125, 45);
+		panel_content.add(labelMes);
+		labelMes.setFont(new Font("Quicksand Medium", Font.PLAIN, 16));
+		
+		JLabel labelData = new JLabel("Segunda - 10/02");
+		labelData.setBounds(20, 43, 125, 22);
+		panel_content.add(labelData);
+		labelData.setFont(new Font("Quicksand Light", Font.PLAIN, 12));
+	}
+	
+	
+	private void configurePanelCalorias(JPanel panel_content) {
 		Panel panel = new Panel();
 		panel.setBounds(296, 90, 334, 139);
 		panel.setBackground(UIManager.getColor("InternalFrame.inactiveTitleGradient"));
@@ -291,14 +116,20 @@ public class TelaHome {
 		lbl_caloriasGastas.setBounds(263, 87, 46, 14);
 		panel.add(lbl_caloriasGastas);
 		
-		return panel;
+		panel_content.add(panel);
 	}
 	
-	public void configureButtonsRefeicoes(JPanel panel) {
+	public void configureButtonsRefeicoes(JPanel panel_content) {
+		// titulo e refeições
+		JLabel lbl_refeicao_title = new JLabel("Refei\u00E7\u00F5es");
+		lbl_refeicao_title.setBounds(32, 291, 125, 33);
+		panel_content.add(lbl_refeicao_title);
+		lbl_refeicao_title.setFont(new Font("Quicksand Medium", Font.PLAIN, 16));
+		
 		// Café da manhã
 		JPanel panel_cafeManha = new JPanel();
 		panel_cafeManha.setBounds(32, 342, 258, 53);
-		panel.add(panel_cafeManha);
+		panel_content.add(panel_cafeManha);
 		panel_cafeManha.setBackground(Color.decode("#CED6E0"));
 		panel_cafeManha.setLayout(null);
 		
@@ -320,7 +151,7 @@ public class TelaHome {
 		labelAdicionarCafe.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				frame.dispose();
+				getFrame().dispose();
 				TelaAlimentos.main(null);
 			}
 		});
@@ -328,7 +159,7 @@ public class TelaHome {
 		// Lanche da manhã
 		JPanel panel_lancheManha = new JPanel();
 		panel_lancheManha.setBounds(332, 342, 258, 53);
-		panel.add(panel_lancheManha);
+		panel_content.add(panel_lancheManha);
 		panel_lancheManha.setLayout(null);
 		panel_lancheManha.setBackground(Color.decode("#CED6E0"));
 		
@@ -349,7 +180,7 @@ public class TelaHome {
 		labelAdicionarLancheManha.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				frame.dispose();
+				getFrame().dispose();
 				TelaAlimentos.main(null);
 			}
 		});
@@ -357,7 +188,7 @@ public class TelaHome {
 		// Almoço
 		JPanel panel_almoco = new JPanel();
 		panel_almoco.setBounds(634, 342, 258, 53);
-		panel.add(panel_almoco);
+		panel_content.add(panel_almoco);
 		panel_almoco.setLayout(null);
 		panel_almoco.setBackground(Color.decode("#CED6E0"));
 		
@@ -378,7 +209,7 @@ public class TelaHome {
 		labelAdicionarAlmoco.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				frame.dispose();
+				getFrame().dispose();
 				TelaAlimentos.main(null);
 			}
 		});
@@ -388,7 +219,7 @@ public class TelaHome {
 		panel_lancheTarde.setBackground(Color.decode("#CED6E0"));
 		panel_lancheTarde.setBounds(32, 422, 258, 53);
 		panel_lancheTarde.setLayout(null);
-		panel.add(panel_lancheTarde);
+		panel_content.add(panel_lancheTarde);
 		
 		JLabel labelCaloriasLancheTarde = new JLabel("0 kcal");
 		labelCaloriasLancheTarde.setFont(new Font("Quicksand Light", Font.PLAIN, 11));
@@ -404,7 +235,7 @@ public class TelaHome {
 		labelAdicionarLancheTarde.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				frame.dispose();
+				getFrame().dispose();
 				TelaAlimentos.main(null);
 			}
 		});
@@ -416,7 +247,7 @@ public class TelaHome {
 		// Janta
 		JPanel panel_janta = new JPanel();
 		panel_janta.setBounds(332, 422, 258, 53);
-		panel.add(panel_janta);
+		panel_content.add(panel_janta);
 		panel_janta.setLayout(null);
 		panel_janta.setBackground(Color.decode("#CED6E0"));
 		
@@ -437,7 +268,7 @@ public class TelaHome {
 		labelAdicionarJanta.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				frame.dispose();
+				getFrame().dispose();
 				TelaAlimentos.main(null);
 			}
 		});
@@ -445,7 +276,7 @@ public class TelaHome {
 		// Lanche da noite
 		JPanel panel_lancheNoite = new JPanel();
 		panel_lancheNoite.setBounds(634, 422, 258, 53);
-		panel.add(panel_lancheNoite);
+		panel_content.add(panel_lancheNoite);
 		panel_lancheNoite.setLayout(null);
 		panel_lancheNoite.setBackground(Color.decode("#CED6E0"));
 		
@@ -466,7 +297,7 @@ public class TelaHome {
 		labelAdicionarLancheNoite.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				frame.dispose();
+				getFrame().dispose();
 				TelaAlimentos.main(null);
 			}
 		});
